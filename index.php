cc199b94eb61096a9071d706d98cc8566a378756
OiNutter <willmckenzie@oinutter.co.uk>
2009-12-30 15:25:30 +0000

    Added sample image and test script to index.php Started refining TextOverlay function

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-    phpinfo();
+    require_once('lib/ImageHandler.php');
+    
+    $img = new ImageHandler("img/Crater.jpg");
+    $img->Resize(array(640,460));
+    $img->TextOverlay("Hello World","arial.ttf","middle",12,0,$img->new_img);
+    $img->Display();
 ?>
