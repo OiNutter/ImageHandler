a7177c9e40d110458f42c21c5f92291ec3f31a34
OiNutter <willmckenzie@oinutter.co.uk>
2009-12-31 11:11:51 +0000

    Added text border function - creates a box of text from a given string Fixed colour getting in TextOverlay function

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-    require_once('lib/ImageHandler.php');
-    
-    $img = new ImageHandler("img/Crater.jpg");
-    $img->Resize(array(640,460));
-    $img->TextOverlay("Hello World","arial.ttf","middle",12,0,$img->new_img);
-    $img->Display();
+<?php 
+
 ?>
